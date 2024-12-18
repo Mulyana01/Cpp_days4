a90f0dfc96fe9a5a2552fd37625975a4c0af7c74
Mulyana <95154862+Mulyana01@users.noreply.github.com>
2024-12-18 22:14:48 +0700

    Updated code star pattern

--- starpattern.cpp
+++ starpattern.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int rows;
 
     // Input jumlah baris
@@ -9,13 +10,21 @@ int main() {
     cin >> rows;
 
     // Membuat pola segitiga sama sisi
-    for (int i = 1; i <= rows; i++) {
+    for (int i = 1; i <= rows; i++)
+    {
         // Print spasi
-        for (int j = 1; j <= rows - i; j++) {
+        for (int j = 1; j <= rows - i; j++)
+        {
             cout << " ";
         }
-//  Print star
 
-for (int k =1 ; k<=input ; k++){
-cout<< "*"<<endl;
-return 0;}
+        //  Print star
+
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
